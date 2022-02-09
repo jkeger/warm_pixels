7a5620b05e433c53b36a4208dd1768d6c38d285e
Richard <richardhayes777@gmail.com>
2022-02-09 12:32:58 +0000

    fixtures to test all combinations

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,9 +1,31 @@
+import pytest
+
 from warm_pixels import WarmPixels
 
 
-def test_integration(mock_dataset):
+@pytest.mark.parametrize(
+    "prep_density",
+    [False, True]
+)
+@pytest.mark.parametrize(
+    "use_corrected",
+    [False, True]
+)
+@pytest.mark.parametrize(
+    "plot_density",
+    [False, True]
+)
+def test_integration(
+        mock_dataset,
+        prep_density,
+        use_corrected,
+        plot_density,
+):
     WarmPixels(
         datasets=[mock_dataset],
         quadrants="A",
         overwrite=True,
+        prep_density=prep_density,
+        use_corrected=use_corrected,
+        plot_density=plot_density,
     ).main()
