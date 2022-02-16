0459017246c7e571a48ca38b7b6e26c5c2da7836
Richard <richardhayes777@gmail.com>
2022-02-16 12:07:59 +0000

    process module; moved file checking responsibility; more polymorphism

--- warm_pixels/__init__.py
+++ warm_pixels/__init__.py
@@ -1,7 +1,4 @@
 import os
-from abc import ABC, abstractmethod
-
-import numpy as np
 
 import warm_pixels.hst_functions.plot
 from warm_pixels import hst_functions as fu
@@ -11,187 +8,8 @@ from warm_pixels.hst_data import Dataset
 from warm_pixels.hst_functions import plot
 from warm_pixels.hst_utilities import output_path
 from warm_pixels.pixel_lines import PixelLine, PixelLineCollection
+from warm_pixels.process import RawProcess, CorrectedProcess
 from warm_pixels.warm_pixels import find_dataset_warm_pixels
-
-
-class AbstractProcess(ABC):
-    def __init__(
-            self,
-            dataset,
-            quadrants,
-            warm_pixels,
-    ):
-        self.dataset = dataset
-        self.warm_pixels = warm_pixels
-        self.quadrants = quadrants
-
-    def run(self):
-        for quadrant in self.quadrants:
-            self.process_quadrant(quadrant)
-
-        self.plot_distributions()
-
-        # Stack warm pixels in each image quadrant or combined quadrants
-        for quadrants in self.quadrants.groups:
-            self.stack_warm_pixels()
-
-    def plot_distributions(self):
-        # Plot distributions of warm pixels in the set
-        if self.warm_pixels.need_to_make_file(
-                self.dataset.plotted_distributions(self.quadrants),
-        ):
-            print("  Distributions of warm pixels...", end=" ", flush=True)
-            fu.plot_warm_pixel_distributions(
-                self.dataset,
-                self.quadrants,
-                save_path=self.dataset.plotted_distributions(self.quadrants),
-            )
-
-    def stack_warm_pixels(self):
-        # Stack in bins
-        if self.warm_pixels.need_to_make_file(
-                self.dataset.saved_stacked_lines(self.quadrants),
-        ):
-            print(
-                f"  Stack warm pixel trails ({''.join(self.quadrants)})...",
-                end=" ",
-                flush=True,
-            )
-            fu.stack_dataset_warm_pixels(self.dataset, self.quadrants)
-
-        # Plot stacked lines
-        if not self.warm_pixels.use_corrected and self.warm_pixels.need_to_make_file(
-                self.dataset.plotted_stacked_trails(
-                    self.quadrants,
-                ),
-        ):
-            print(
-                f"  Plot stacked trails ({''.join(self.quadrants)})...",
-                end=" ",
-                flush=True,
-            )
-            fu.plot_stacked_trails(
-                self.dataset,
-                self.quadrants,
-                use_corrected=self.warm_pixels.use_corrected,
-                save_path=self.dataset.plotted_stacked_trails(
-                    self.quadrants
-                ),
-            )
-
-    @abstractmethod
-    def process_quadrant(self, quadrant):
-        pass
-
-
-class RawProcess(AbstractProcess):
-    def process_quadrant(self, quadrant):
-        # Find possible warm pixels in each image
-        if self.warm_pixels.need_to_make_file(
-                self.dataset.saved_lines(quadrant),
-        ):
-            print(
-                f"  Find possible warm pixels ({quadrant})...",
-                end=" ",
-                flush=True,
-            )
-            find_dataset_warm_pixels(self.dataset, quadrant)
-
-        # Find consistent warm pixels in the set
-        if self.warm_pixels.need_to_make_file(
-                self.dataset.saved_consistent_lines(quadrant),
-        ):
-            print(
-                f"  Consistent warm pixels ({quadrant})...",
-                end=" ",
-                flush=True,
-            )
-            fu.find_consistent_warm_pixels(
-                self.dataset,
-                quadrant,
-                flux_min=ut.flux_bins[0],
-                flux_max=ut.flux_bins[-1],
-            )
-
-
-class CorrectedProcess(AbstractProcess):
-    def __init__(
-            self,
-            raw_process
-    ):
-        super().__init__(
-            dataset=raw_process.dataset.corrected(),
-            warm_pixels=raw_process.warm_pixels,
-            quadrants=raw_process.quadrants,
-        )
-        self.raw_process = raw_process
-
-    def process_quadrant(self, quadrant):
-        self.raw_process.process_quadrant(quadrant)
-
-        # Extract from corrected images with CTI removed
-        if self.warm_pixels.need_to_make_file(
-                self.dataset.saved_consistent_lines(quadrant),
-        ):
-            print(f"  Extract CTI-removed warm pixels ({quadrant})...")
-            self.extract_consistent_warm_pixels_corrected(quadrant)
-
-    def extract_consistent_warm_pixels_corrected(self, quadrant):
-        """Extract the corresponding warm pixels from the corrected images with CTI
-        removed, in the same locations as the orignal consistent warm pixels.
-
-        Parameters
-        ----------
-        quadrant : str (opt.)
-            The quadrant (A, B, C, D) of the image to load.
-
-        Saves
-        -----
-        warm_pixels_cor : PixelLineCollection
-            The set of consistent warm pixel trails, saved to
-            dataset.saved_consistent_lines(use_corrected=True).
-        """
-        # Load original warm pixels for the whole dataset
-        warm_pixels = PixelLineCollection()
-        warm_pixels.load(self.raw_process.dataset.saved_consistent_lines(quadrant))
-
-        # Corrected images
-        warm_pixels_cor = PixelLineCollection()
-        for i, image in enumerate(self.dataset):
-            image_name = image.name
-            print(
-                f"\r    {image_name}_cor_{quadrant} ({i + 1} of {len(self.dataset)}) ",
-                end="",
-                flush=True,
-            )
-
-            # Load the image
-            array = image.load_quadrant(quadrant)
-
-            # Select consistent warm pixels found from this image
-            image_name_q = image_name + "_%s" % quadrant
-            sel = np.where(warm_pixels.origins == image_name_q)[0]
-            for i in sel:
-                line = warm_pixels.lines[i]
-                row, column = line.location
-
-                # Copy the original metadata but take the data from the corrected image
-                warm_pixels_cor.append(
-                    PixelLine(
-                        data=array[
-                             row - ut.trail_length: row + ut.trail_length + 1, column
-                             ],
-                        origin=line.origin,
-                        location=line.location,
-                        date=line.date,
-                        background=line.background,
-                    )
-                )
-
-        print("Extracted %d lines" % warm_pixels_cor.n_lines)
-
-        # Save
-        warm_pixels_cor.save(self.dataset.saved_consistent_lines(quadrant))
 
 
 class WarmPixels:
@@ -214,11 +32,6 @@ class WarmPixels:
         # TODO: list name was originally the input...
         self.list_name = "test"
 
-    def need_to_make_file(self, filename):
-        if self.overwrite:
-            return True
-        return not os.path.exists(filename)
-
     def main(self):
         # ========
         # Create directories to contain output plots
@@ -240,7 +53,7 @@ class WarmPixels:
             process = RawProcess(
                 dataset,
                 self.quadrants,
-                self,
+                overwrite=self.overwrite,
             )
             if self.use_corrected:
                 process = CorrectedProcess(process)
