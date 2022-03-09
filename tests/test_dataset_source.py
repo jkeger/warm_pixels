7fe043a8ec60c47751191fa0afb15cd4e4ca655c
Richard <richardhayes777@gmail.com>
2022-03-09 16:56:51 +0000

    filters with names

--- tests/test_dataset_source.py
+++ tests/test_dataset_source.py
@@ -9,14 +9,27 @@ from warm_pixels import Dataset
 
 
 class AbstractDatasetSource:
+    @property
+    @abstractmethod
+    def name(self):
+        pass
+
     @abstractmethod
     def datasets(self):
         pass
 
     def after(self, date: dt.date):
         return FilteredDatasetSource(
+            f"after_{date}",
             self,
             lambda dataset: date < dataset.observation_date()
+        )
+
+    def before(self, date: dt.date):
+        return FilteredDatasetSource(
+            f"before_{date}",
+            self,
+            lambda dataset: dataset.observation_date() < date
         )
 
 
@@ -28,6 +41,10 @@ class FileDatasetSource(AbstractDatasetSource):
     ):
         self.directory = directory
         self.output_path = output_path
+
+    @property
+    def name(self):
+        return self.directory.name
 
     def datasets(self):
         dataset_folders = os.listdir(
@@ -45,22 +62,24 @@ class FileDatasetSource(AbstractDatasetSource):
 class FilteredDatasetSource(AbstractDatasetSource):
     def __init__(
             self,
+            filter_name,
             source,
-            *filters
+            filter_
     ):
+        self.filter_name = filter_name
         self.source = source
-        self.filters = filters
+        self.filter = filter_
+
+    @property
+    def name(self):
+        return f"{self.source.name}_{self.filter_name}"
 
     def datasets(self):
         return [
             dataset
             for dataset
             in self.source.datasets()
-            if all(
-                filter_(dataset)
-                for filter_
-                in self.filters
-            )
+            if self.filter(dataset)
         ]
 
 
@@ -81,20 +100,42 @@ def test_dataset_source(
         source
 ):
     assert len(source.datasets()) == 1
+    assert source.name == "dataset_list"
 
 
 @pytest.mark.parametrize(
-    "date, count",
+    "date, count, name",
     [
-        (dt.date(2019, 12, 30), 1),
-        (dt.date(2020, 1, 1), 0),
-        (dt.date(2020, 10, 1), 0),
+        (dt.date(2019, 12, 30), 1, "dataset_list_after_2019-12-30"),
+        (dt.date(2020, 1, 1), 0, "dataset_list_after_2020-01-01"),
+        (dt.date(2020, 10, 1), 0, "dataset_list_after_2020-10-01"),
     ]
 )
-def test_filter(
+def test_after(
         source,
         date,
-        count
+        count,
+        name,
 ):
     source = source.after(date)
     assert len(source.datasets()) == count
+    assert source.name == name
+
+
+@pytest.mark.parametrize(
+    "date, count, name",
+    [
+        (dt.date(2019, 12, 30), 0, "dataset_list_before_2019-12-30"),
+        (dt.date(2020, 1, 1), 0, "dataset_list_before_2020-01-01"),
+        (dt.date(2020, 10, 1), 1, "dataset_list_before_2020-10-01"),
+    ]
+)
+def test_before(
+        source,
+        date,
+        count,
+        name,
+):
+    source = source.before(date)
+    assert len(source.datasets()) == count
+    assert source.name == name
