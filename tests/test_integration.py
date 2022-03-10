c155c809a9015fd0dfa560d18cd1c7e897e42a5c
Richard <richardhayes777@gmail.com>
2022-03-10 15:31:24 +0000

    fixed integration test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,37 +1,34 @@
+import pytest
+
 from warm_pixels import WarmPixels
-from warm_pixels.plot import output_plots
+from warm_pixels.plot import Plot
 
 
+@pytest.mark.parametrize(
+    "use_corrected",
+    [False, True]
+)
 def test_integration(
         dataset,
         savefig_calls,
+        use_corrected,
 ):
+    if use_corrected:
+        dataset = dataset.corrected()
+
     warm_pixels = WarmPixels(
         datasets=[dataset, dataset],
         quadrants="A",
     )
-    output_plots(
-        warm_pixels,
+    plot = Plot(
+        warm_pixels_=warm_pixels,
+        use_corrected=use_corrected,
         list_name="test",
-        plot_warm_pixels=True,
-        plot_density=True,
     )
-    assert len(savefig_calls) == 7
-
-
-def test_integration_corrected(
-        dataset,
-        savefig_calls,
-):
-    dataset = dataset.corrected()
-    warm_pixels = WarmPixels(
-        datasets=[dataset, dataset],
-        quadrants="A",
-    )
-    output_plots(
-        warm_pixels,
-        list_name="test",
-        plot_warm_pixels=True,
-        plot_density=True,
-    )
+    plot.by_name([
+        "warm-pixels",
+        "warm-pixel-distributions",
+        "stacked-trails",
+        "density",
+    ])
     assert len(savefig_calls) == 7
