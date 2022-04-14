beff8b7c370423b8f8de67058360e8174826f7ae
Richard <richardhayes777@gmail.com>
2022-04-14 14:19:04 +0100

    extra test for persistence

--- tests/test_persist.py
+++ tests/test_persist.py
@@ -13,11 +13,31 @@ def make_quadrant(dataset):
     )
 
 
+@pytest.fixture(
+    name="corrected_quadrant"
+)
+def make_corrected_quadrant(dataset):
+    return Quadrant(
+        dataset=dataset.corrected(),
+        quadrant="A"
+    )
+
+
 def test_stacked_lines(quadrant, output_path):
     group = QuadrantGroup([quadrant])
     group.stacked_lines()
 
     assert (output_path / "dataset" / "A" / "stacked_lines.pickle").exists()
+
+
+def test_stacked_lines_corrected(
+        corrected_quadrant,
+        output_path
+):
+    group = QuadrantGroup([corrected_quadrant])
+    group.stacked_lines()
+
+    assert (output_path / "dataset_corrected" / "A" / "stacked_lines.pickle").exists()
 
 
 def test_consistent_lines(quadrant, output_path):
@@ -26,12 +46,10 @@ def test_consistent_lines(quadrant, output_path):
     assert (output_path / "dataset" / "A" / "consistent_lines.pickle").exists()
 
 
-def test_consistent_lines_corrected(dataset, output_path):
-    quadrant = Quadrant(
-        dataset=dataset.corrected(),
-        quadrant="A"
-    )
-
-    quadrant.consistent_lines()
+def test_consistent_lines_corrected(
+        corrected_quadrant,
+        output_path
+):
+    corrected_quadrant.consistent_lines()
 
     assert (output_path / "dataset_corrected" / "A" / "consistent_lines.pickle").exists()
