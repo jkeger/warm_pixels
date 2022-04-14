ec60c86990d2cd714e58e869c11d80fb6354e434
Richard <richardhayes777@gmail.com>
2022-04-14 09:43:31 +0100

    persist output from all_trap_densities

--- tests/test_persist.py
+++ tests/test_persist.py
@@ -1,20 +1,26 @@
 import pytest
 
-from warm_pixels import Quadrant, QuadrantGroup
+import warm_pixels as wp
 
 
 @pytest.fixture(
     name="quadrant"
 )
 def make_quadrant(dataset):
-    return Quadrant(
+    return wp.Quadrant(
         dataset=dataset,
         quadrant="A"
     )
 
 
+def test_warm_pixels_directory_function(dataset):
+    assert wp.directory_func(
+        wp.WarmPixels([dataset])
+    ) == "380a702f2184cc52bc1fadd47fb07f44"
+
+
 def test_stacked_lines(quadrant, output_path):
-    group = QuadrantGroup([quadrant])
+    group = wp.QuadrantGroup([quadrant])
     group.stacked_lines()
 
     assert (output_path / "dataset" / "A" / "stacked_lines.pickle").exists()
@@ -27,7 +33,7 @@ def test_consistent_lines(quadrant, output_path):
 
 
 def test_consistent_lines_corrected(dataset, output_path):
-    quadrant = Quadrant(
+    quadrant = wp.Quadrant(
         dataset=dataset.corrected(),
         quadrant="A"
     )
