830b7afe105c71d2e127a6b68e634370cc1d5d59
Richard <richardhayes777@gmail.com>
2022-05-30 08:42:31 +0100

    demonstration that stacked lines can be serialised too

--- tests/test_pixel_line_export.py
+++ tests/test_pixel_line_export.py
@@ -1,10 +1,15 @@
 import numpy as np
+import pytest
 
 from warm_pixels import PixelLine
+from warm_pixels.pixel_lines.stacked_collection import StackedPixelLine
 
 
-def test_to_dict():
-    pixel_line = PixelLine(
+@pytest.fixture(
+    name="pixel_line"
+)
+def make_pixel_line():
+    return PixelLine(
         location=[26, 25],
         date=20.5,
         background=23,
@@ -13,6 +18,8 @@ def test_to_dict():
         noise=np.array([2.0, 3.0, 4.0])
     )
 
+
+def test_to_dict(pixel_line):
     assert pixel_line.dict == {
         'background': 23,
         'data': [1., 4., 5.],
@@ -20,4 +27,24 @@ def test_to_dict():
         'flux': 150,
         'location': [26, 25],
         'noise': [2., 3., 4.]
+    }
+
+
+def test_stacked_pixel_line(pixel_line):
+    stacked = StackedPixelLine(
+        length=3,
+        background=10,
+        date=1000,
+        location=[0, 30]
+    )
+    stacked.append(pixel_line)
+    stacked.append(pixel_line)
+
+    assert stacked.dict == {
+        'background': 10,
+        'data': [1., 4., 5.],
+        'date': 1000,
+        'flux': 5.0,
+        'location': [0, 30],
+        'noise': [0.0, 0.0, 0.0]
     }
