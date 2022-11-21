374d27f62df8c842973736cd4cf215898f4d2c7a
Richard <richardhayes777@gmail.com>
2022-11-21 10:28:37 +0000

    unused commented code

--- warm_pixels/bins.py
+++ warm_pixels/bins.py
@@ -88,6 +88,3 @@ class Bins:
                 f"Value {value} is greater than upper bound of max bin {self.max}"
             )
         return np.digitize(value, self[:-1]) - 1
-
-    # def __getattr__(self, item):
-    #     return self.bins[item]
